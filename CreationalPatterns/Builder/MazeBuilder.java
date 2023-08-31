545d2dcf2cb2da4ca1900fcece0206829994d016
Dominik Panzarella <131593612+DominikPanzarella@users.noreply.github.com>
2023-08-31 17:38:38 +0200

    Add files via upload

---
+++ CreationalPatterns/Builder/MazeBuilder.java
@@ -0,0 +1,17 @@
+package Builder;
+
+import CommonClasses.Maze;
+
+public abstract class MazeBuilder
+{
+
+    protected MazeBuilder(){ }
+
+    public void buildMaze(){ }
+
+    public void buildDoor(final int roomFrom, final int roomTo){ }
+
+    public Maze getMaze(){ return null; }
+
+    public void buildRoom(final int roomNumber){ }
+}
